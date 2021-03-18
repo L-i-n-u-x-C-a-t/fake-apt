7ad9714c2c4d1134d773afd28f1fc1bb91f4a692
LinuxCat <masch77.linuxcat@gmail.com>
2021-03-18 21:18:46 +0100

    like really pls kill me

--- fake-apt-3ds/keyboard.hpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-#include <vector>
-#include <3ds.h>
-#include <3ds/types.h>
-static std::vector<SwkbdDictWord> words;
-std::string setkbdString(uint maxLength, const std::string &Text);
